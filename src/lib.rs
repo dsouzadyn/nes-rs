00513434eb0e5d96ed3a5dcacc1152981e5bcda3
Dylan Dsouza <dsouzadyn@gmail.com>
2022-12-26 21:33:44 +0530

    complete first set of instructions

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub struct CPU {
     pub register_a: u8,
     pub status: u8,
     pub program_counter: u16,
+    pub register_x: u8,
 }
 
 impl CPU {
@@ -10,7 +11,41 @@ impl CPU {
             register_a: 0,
             status: 0,
             program_counter: 0,
+            register_x: 0,
         }
+    }
+    // flag updation
+    fn update_zero_and_negative_flag(&mut self, result: u8) {
+        if result == 0 {
+            self.status = self.status | 0b0000_0010;
+        } else {
+            self.status = self.status & 0b1111_1101;
+        }
+        // set negative flag
+        if result & 0b1000_0000 != 0 {
+            self.status = self.status | 0b1000_0000;
+        } else {
+            self.status = self.status & 0b0111_1111;
+        }
+    }
+    // LDA
+    fn lda(&mut self, value: u8) {
+        self.register_a = value;
+        self.update_zero_and_negative_flag(self.register_a);
+    }
+    // TAX
+    fn tax(&mut self) {
+        self.register_x = self.register_a;
+        self.update_zero_and_negative_flag(self.register_x);
+    }
+    // INX
+    fn inx(&mut self) {
+        if self.register_x == 0xff {
+            self.register_x = 0;
+        } else {
+            self.register_x = self.register_x + 1;
+        }
+        self.update_zero_and_negative_flag(self.register_x);
     }
     // Status Flags -> NV_BDIZC
     pub fn interpret(&mut self, program: Vec<u8>) {
@@ -22,20 +57,10 @@ impl CPU {
                 0xA9 => {
                     let param = program[self.program_counter as usize];
                     self.program_counter += 1;
-                    self.register_a = param;
-                    // set zero flag
-                    if self.register_a == 0 {
-                        self.status = self.status | 0b0000_0010;
-                    } else {
-                        self.status = self.status & 0b1111_1101;
-                    }
-                    // set negative flag
-                    if self.register_a & 0b1000_0000 != 0 {
-                        self.status = self.status | 0b1000_0000;
-                    } else {
-                        self.status = self.status & 0b0111_1111;
-                    }
+                    self.lda(param);
                 }
+                0xAA => self.tax(),
+                0xE8 => self.inx(),
                 0x00 => {
                     return;
                 }
@@ -53,6 +78,7 @@ mod test {
     fn test_0xa9_lda_immidiate_load_data() {
         let mut cpu = CPU::new();
         cpu.interpret(vec![0xa9, 0x05, 0x00]);
+
         assert_eq!(cpu.register_a, 0x05);
         assert!(cpu.status & 0b0000_0010 == 0);
         assert!(cpu.status & 0b1000_0000 == 0);
@@ -62,6 +88,40 @@ mod test {
     fn test_0xa9_lda_zero_flag() {
         let mut cpu = CPU::new();
         cpu.interpret(vec![0xa9, 0x00, 0x00]);
-        assert!(cpu.status & 0b0000_0010 == 0b10);
+
+        assert!(cpu.status & 0b0000_0010 == 0b10)
+    }
+
+    #[test]
+    fn test_0xaa_tax_move_a_to_x() {
+        let mut cpu = CPU::new();
+        cpu.register_a = 10;
+        cpu.interpret(vec![0xaa, 0x00]);
+
+        assert_eq!(cpu.register_x, 10)
+    }
+
+    #[test]
+    fn test_0xe8_inx_increment_x() {
+        let mut cpu = CPU::new();
+        cpu.register_x = 2;
+        cpu.interpret(vec![0xe8, 0x00]);
+        assert_eq!(cpu.register_x, 3)
+    }
+
+    #[test]
+    fn test_0xe8_inx_overflow() {
+        let mut cpu = CPU::new();
+        cpu.register_x = 0xff;
+        cpu.interpret(vec![0xe8, 0xe8, 0x00]);
+        assert_eq!(cpu.register_x, 1)
+    }
+
+    #[test]
+    fn test_5_ops_working_together() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+
+        assert_eq!(cpu.register_x, 0xc1)
     }
 }
